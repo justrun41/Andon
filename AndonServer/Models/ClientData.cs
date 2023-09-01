39ddb332c1ecb7049eefd5b4ca2040dbe314fae8
Andrew Williams <asml4@live.com>
2023-09-01 16:30:03 -0400

    Testing

--- AndonServer/Models/ClientData.cs
+++ AndonServer/Models/ClientData.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace AndonServer.Models
 {
      internal class ClientData : IEquatable<ClientData>
     {
+        [BsonId]
+
+        [BsonRepresentation(BsonType.ObjectId)]
         public required string ComputerName { get; set; }
         public string? ColorCode { get; set; }
         public int XCoordinate { get; set; }
